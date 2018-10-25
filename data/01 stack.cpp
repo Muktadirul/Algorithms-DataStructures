4830c07d39d410a7fedb04d7c8681ef469b9a357
Muktadirul <raiancse@gmail.com>
2018-10-26 03:26:00 +0600

    stack implemented

--- data/01 stack.cpp
+++ data/01 stack.cpp
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+int arr[15];
+int top=0;
+
+void push(int data){
+    if (top < 15){
+        arr[top] = data;
+        top++;
+        printf("%d pushed in stack\n",data);
+        }
+    else
+        printf("Stack Overflow\n");
+}
+
+void pop(){
+
+    if (top >=0 ){
+        printf("array %d \n",arr[top]);
+        top--;
+    }
+    else
+        printf("Stack Underflow\n");
+
+}
+
 int main(){
+    for (int i=0;i<20;i++)
+        push(i);
+    for(int i=0;i<20;i++){
+        pop();
+    }
 
 
-    printf("Hello\n");
     return 0;
 }
